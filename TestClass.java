50186eef800a6dd0c77f807aa97deee4dd28be43
Akhil-teja1 <91177808+Akhil-teja1@users.noreply.github.com>
2022-08-28 18:26:34 +0530

    A test class

---
+++ TestClass.java
@@ -0,0 +1,16 @@
+package com.springapplication;  
+  
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.ClassPathResource;  
+import org.springframework.core.io.Resource;  
+  
+public class TestClass {  
+public static void main(String[] args) {  
+    Resource resource=new ClassPathResource("applicationContext.xml");  
+    BeanFactory factory=new XmlBeanFactory(resource);  
+      
+    BasicSpringApplication basicSpringApplication=(BasicSpringApplication)factory.getBean("studentbean");  
+    basicSpringApplication.displayInfo();  
+}  
+}  
